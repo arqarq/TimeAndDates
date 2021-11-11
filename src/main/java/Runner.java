b48e7494d3d7b8bc50d7481d3f81db99bfc9ec05
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2021-11-11 23:09:30 +0100

    ...

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,41 +1,47 @@
 import org.threeten.bp.Instant;
 import org.threeten.bp.LocalDate;
 import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.LocalTime;
 import org.threeten.bp.OffsetDateTime;
 import org.threeten.bp.ZoneId;
 import org.threeten.bp.ZoneOffset;
 import org.threeten.bp.format.DateTimeFormatter;
-import pl.prokom.epwd.utilLib.toStr.ToStr;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
-public class Runner {
-    public static void main(String[] args) throws ParseException {
-        Date now = new Date(2021, Calendar.OCTOBER, 10, 12, 55, 55);
-        GregorianCalendar gregorianCalendar = new GregorianCalendar(2021, Calendar.OCTOBER, 10, 12, 55, 55);
-        now = gregorianCalendar.getTime();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2021, Calendar.OCTOBER, 15, 0, 0, 55);
-        now = calendar.getTime();
-        SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd");
+import static org.threeten.bp.temporal.ChronoUnit.DAYS;
 
+public class Runner {
+    public static void main(String[] args) {
+        SimpleDateFormat sm = new SimpleDateFormat("yyyy-MM-dd");
+        GregorianCalendar gregorianCalendar = new GregorianCalendar(2021, Calendar.OCTOBER, 15, 0, 55, 55);
+
+        Date now = gregorianCalendar.getTime();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2021, Calendar.NOVEMBER, 15, 0, 55, 55);
+        Date now2 = calendar.getTime();
         String strDate = sm.format(now);
+        String strDate2 = sm.format(now2);
         LocalDate wymagalnoscLocalDate = LocalDate.parse(strDate);
         OffsetDateTime wymagalnoscOffsetDateTime = OffsetDateTime.parse(strDate + "T00:00:00+01:00");
+        OffsetDateTime wymagalnoscOffsetDateTime2 = OffsetDateTime.parse(strDate2 + "T00:00:00+01:00");
         long epochMilli = now.getTime();
+        long epochMilli2 = now2.getTime();
         OffsetDateTime offsetDateTime = OffsetDateTime.of(
-                LocalDateTime.ofInstant(Instant.ofEpochMilli(epochMilli), ZoneId.systemDefault()),
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(epochMilli), ZoneId.systemDefault()).truncatedTo(DAYS),
                 ZoneOffset.ofTotalSeconds(TimeZone.getDefault().getOffset(epochMilli) / 1000));
-                // ZoneOffset.ofHours(1));
-        System.out.println(ToStr.logAs("XXX compare", wymagalnoscOffsetDateTime.toString() + " / " + offsetDateTime));
-        // System.out.println(ToStr.logAs("XXX compare", wymagalnoscOffsetDateTime + " / " + offsetDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneId.systemDefault()))));
-        System.out.println(ToStr.logAs("XXX compare", wymagalnoscOffsetDateTime + " / " + offsetDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE.withZone(ZoneId.of("America/El_Salvador")))));
-        System.out.println(ZoneId.getAvailableZoneIds());
-        System.out.println(TimeZone.getDefault().getOffset(epochMilli) / 1000);
+        OffsetDateTime offsetDateTime2 = OffsetDateTime.of(
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(epochMilli2), ZoneId.systemDefault()).toLocalDate(),
+                LocalTime.ofSecondOfDay(0),
+                // LocalTime.of(0, 0),
+                ZoneOffset.ofHours(TimeZone.getDefault().getOffset(epochMilli2) / 1000 / 3600));
+        System.out.println("compare : " + wymagalnoscOffsetDateTime + " / " + offsetDateTime);
+        System.out.println("compare : " + wymagalnoscOffsetDateTime + " / " + offsetDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        System.out.println("compare2: " + wymagalnoscOffsetDateTime2 + " / " + offsetDateTime2);
+        System.out.println("compare2: " + wymagalnoscOffsetDateTime2 + " / " + offsetDateTime2.format(DateTimeFormatter.ISO_LOCAL_DATE));
     }
 }
